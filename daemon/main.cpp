f8982ed806bd1c0bcb6e24d93f95733a3ff4f968
Artur Gilmutdinov <arturg000@gmail.com>
2014-10-03 22:09:10 +0400

    Fix

--- daemon/main.cpp
+++ daemon/main.cpp
@@ -1,7 +1,15 @@
 #include "hiconfig.h"
 #include "hashd.h"
 
+void signal_callback_handler(int signum) {
+	
+	int status;
+	wait(&status);
+}
+
 int main (int argc, char **argv) {
+
+	signal(SIGCHLD, signal_callback_handler);
 	
 	Hashd *h = new Hashd("hashd.conf");
 	h->start();

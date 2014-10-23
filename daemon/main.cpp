f9bc6d3040cce18af2182f1a40dcc65663d80d30
Artur Gilmutdinov <arturg000@gmail.com>
2014-10-23 18:19:10 +0400

    fix sigpipe

--- daemon/main.cpp
+++ daemon/main.cpp
@@ -7,9 +7,15 @@ void signal_callback_handler(int signum) {
 	wait(&status);
 }
 
+void empty_handler(int signal) {
+	
+	//std::cout << "empty handler\n";
+}
+
 int main (int argc, char **argv) {
 
 	signal(SIGCHLD, signal_callback_handler);
+	signal(SIGPIPE, empty_handler);
 	
 	Hashd *h = new Hashd("hashd.conf");
 	h->start();
